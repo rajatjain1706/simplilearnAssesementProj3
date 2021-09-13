b40f5eee8eca364903decaac1578695f7078a016
rajatjain1706 <rajatranujain2010@gmail.com>
2021-09-13 16:34:30 +0000

    added new code V2.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
  public static void main(String[] args)
   {
       System.out.println("Hello World created by Rajat");
+     System.out.println("This project is edited by Rajat..Version2");
   }
 }
